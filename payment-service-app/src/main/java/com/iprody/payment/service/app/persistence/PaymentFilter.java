c210a329babf7abf11e8016c1235caad36503485
khumoyun_isokov <isakovhumoun71@gmail.com>
2025-12-11 19:46:53 +0300

    Add filtering, sorting and pagination for payments

--- payment-service-app/src/main/java/com/iprody/payment/service/app/persistence/PaymentFilter.java
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/persistence/PaymentFilter.java
@@ -1,8 +1,10 @@
 package com.iprody.payment.service.app.persistence;
 
+import lombok.Data;
+
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
-
+@Data
 public class PaymentFilter {
     private String currency;
     private BigDecimal minAmount;
@@ -10,52 +12,4 @@ public class PaymentFilter {
     private OffsetDateTime createdAfter;
     private OffsetDateTime createdBefore;
     private String status;
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
-    public BigDecimal getMinAmount() {
-        return minAmount;
-    }
-
-    public void setMinAmount(BigDecimal minAmount) {
-        this.minAmount = minAmount;
-    }
-
-    public BigDecimal getMaxAmount() {
-        return maxAmount;
-    }
-
-    public void setMaxAmount(BigDecimal maxAmount) {
-        this.maxAmount = maxAmount;
-    }
-
-    public OffsetDateTime getCreatedAfter() {
-        return createdAfter;
-    }
-
-    public void setCreatedAfter(OffsetDateTime createdAfter) {
-        this.createdAfter = createdAfter;
-    }
-
-    public OffsetDateTime getCreatedBefore() {
-        return createdBefore;
-    }
-
-    public void setCreatedBefore(OffsetDateTime createdBefore) {
-        this.createdBefore = createdBefore;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 }
